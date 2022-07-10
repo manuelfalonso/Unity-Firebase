f3912d0d17417e4221ca71a345f03b2a736a38be
Manuel Francisco Alonso <manuelalonso89@gmail.com>
2022-07-09 22:15:02 -0300

    ChildChanged Realtime Database added

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ Firebase example project in Unity
 * Unity 2021.3.1f1 LTS
 * Visual Studio Community 2022
 
+## Roadmap
+
+* Authentication (finished)
+* Firestore Database
+* Realtime Database (in progress)
+* Cloud Storage
+* Cloud Functions
+* Remote Config
+* A/B Testing
+* Google Ads
+* Hosting
+* More...
+
+* Web pages
+* Language translation
+
 ## Usage
 
 * Open project with Unity Hub
--- Unity-Firebase/Assets/Sombra Studios/Firebase/Realtime Database/Save Data/SaveDataController.cs
+++ Unity-Firebase/Assets/Sombra Studios/Firebase/Realtime Database/Save Data/SaveDataController.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using Firebase.Database;
 using UnityEngine;
+using TMPro;
 
 public class SaveDataController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TMP_InputField _userId;
+    [SerializeField] private TMP_InputField _userUsername;
+    [SerializeField] private TMP_InputField _userEmail;
+
+    private void Start()
     {
-        
+        DatabaseController.Instance.DatabaseReference.
+            Child("users").ChildChanged += HandleValueChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveData()
     {
-        
+        User user = new User(_userUsername.text, _userEmail.text);
+        string json = JsonUtility.ToJson(user);
+        Debug.Log(JsonUtility.ToJson(user, true));
+        DatabaseController.Instance.DatabaseReference.
+            Child("users").Child(_userId.text).SetRawJsonValueAsync(json);
+    }
+
+    private void HandleValueChanged(object sender, ChildChangedEventArgs e)
+    {
+        var json = e.Snapshot.GetRawJsonValue();
+        if (!string.IsNullOrEmpty(json))
+        {
+            var playerData = JsonUtility.FromJson<User>(json);
+            _userUsername.text = playerData.username;
+            _userEmail.text = playerData.email;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        DatabaseController.Instance.DatabaseReference.
+            Child("users").ChildChanged -= HandleValueChanged;
     }
 }
